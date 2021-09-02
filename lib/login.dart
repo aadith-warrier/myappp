9098e47f1d8c734a8fdcfb2b9dad0fd154939826
aadith-warrier <warrieraadith@gmail.com>
2021-09-02 19:21:55 +0530

    added login page

---
+++ lib/login.dart
@@ -0,0 +1,111 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+
+class Login extends StatefulWidget {
+  @override
+  _LoginState createState() => _LoginState();
+}
+
+class _LoginState extends State<Login> {
+
+  int counter=1;
+
+  @override
+  Widget build(BuildContext) {
+    return Scaffold(
+      backgroundColor: Color(0xff8660F0),
+      body: ListView(
+        children:[ Column(
+          children: [
+            Container(
+              padding: EdgeInsets.fromLTRB(105, 230, 105, 70),
+              child: Text("Welcome.",
+                style: TextStyle(
+                  fontWeight: FontWeight.w600,
+                  color: Colors.black,
+                  fontSize: 44,
+                ),
+              ),
+            ),
+            Container(
+              padding: EdgeInsets.fromLTRB(40, 00, 40, 10),
+              child: Column(
+                children: [
+                  Container(
+                    decoration: BoxDecoration(
+                      color: Color(0xff67ef45),
+                      borderRadius: BorderRadius.all(Radius.circular(25)),
+                    ),
+                    child: Column(
+                      children: [
+                        Container(
+                            padding:EdgeInsets.fromLTRB(40, 30, 40, 8),
+                            child: TextField(
+                              decoration: InputDecoration(
+                                border: OutlineInputBorder(
+                                  borderRadius: BorderRadius.circular(25),
+                                ),
+                                filled: true,
+                                hintStyle: TextStyle(color: Colors.grey.shade400,fontWeight: FontWeight.w400),
+                                hintText: "Username or E-Mail ID",
+                                fillColor: Colors.white,
+                              ),
+                            )
+                        ),
+                        Container(
+                            padding:EdgeInsets.fromLTRB(40, 20, 40, 8),
+                            child: TextField(
+                              obscureText: true,
+                              decoration: InputDecoration(
+                                border: OutlineInputBorder(
+                                  borderRadius: BorderRadius.circular(25),
+                                ),
+                                filled: true,
+                                hintStyle: TextStyle(color: Colors.grey.shade400,fontWeight: FontWeight.w400),
+                                hintText: "Password",
+                                fillColor: Colors.white,
+                              ),
+                            )
+                        ),
+                        Container(
+                          padding: EdgeInsets.fromLTRB(40, 20, 40, 20),
+                          child: ElevatedButton(
+                            style: ButtonStyle(
+                              backgroundColor: MaterialStateProperty.all(Color(0xff8660F0)),
+                              shape: MaterialStateProperty.all(RoundedRectangleBorder(borderRadius: BorderRadius.circular(25),
+                              ),
+                              ),
+                            ),
+                            onPressed: null,
+                            child: Text("     Login     ",
+                              style: TextStyle(
+                                color: Colors.white,
+                                fontWeight: FontWeight.bold,
+                                fontSize: 24,
+                              ),
+                            ),
+                          ),
+                        ),
+                      ],
+
+                    ),
+                  ),
+                  Container(
+                    padding:EdgeInsets.fromLTRB(00, 8, 00, 00),
+                    child:RichText(
+                      text: TextSpan(text: "Not Registered? Register here.",
+                      ),
+
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
+        ],
+      ),
+    );
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:myappp/login.dart';
+
+void main() {
+  runApp(MaterialApp(
+    routes:{
+      '/':(context)=>Login(),
+    }
+  ));
+  }
+
+
+
+
